408de27734d7d3645b4437ab59ddb382a7230b48
rainsirendi <rainsirendi@gmail.com>
2017-05-12 11:28:45 +0300

    Rest korrektsemalt kirjutatud

--- Rain/TS/app/home.ts
+++ Rain/TS/app/home.ts
@@ -1,52 +1,44 @@
-/// <reference path="animals.ts"/>
-/// <reference path="helper.ts"/>
-/// <reference path="page.ts"/>
+/// <reference path='animals.ts' /> 
+/// <reference path='helper.ts' /> 
+/// <reference path='page.ts' />
 interface Greeting{
     id: number,
-     content: string
+    content: string
 }
-
 class Home extends Page {
         constructor() {
-        super()  //kutsub välja Page construktori
+        super()
         this._cacheDOM();
         this._bindEvents();
         this._render();
-        
     }
-    
-        
     private _template : string;
-    private _restAnswer : string;
+    private _restJSON : Greeting;
     private _homeModule : Element;
     private _button : HTMLButtonElement;
     private _list : Element;
-
-    protected _cacheDOM(){
-             this._template = Helper.getHTMLTemplate("templates/home-template.htm");
-        this._homeModule = document.querySelector('main');  // otsib üles index.html-is <main> ja asendab galeriiga
-        this._homeModule.outerHTML = this._template;   //kirjutame Module Template-iga üle
-        this._homeModule = document.getElementById('home');  // muudab picsModule lõplikult ära selles cache-s
-        // this._restAnswer = Helper.getHTMLTemplate('http://rest-service.guides.spring.io/greeting'); // võtab ühe pildi template teiste template-de seest
+    
+    protected _cacheDOM() {
+        this._template = Helper.getHTMLTemplate("templates/home-template.htm");
+        this._homeModule = document.querySelector('main');
+        this._homeModule.outerHTML = this._template;
+        this._homeModule = document.getElementById('home');
         this._button = this._homeModule.querySelector('#refresh') as HTMLButtonElement;
-        
-        
-        this._list = this._homeModule.querySelector('#restOutput');     //koostab listi mida kuvada
-        let animals = new Animals();  //Peab olema reast 23 allpool - !!! Animals class ise juba kuvab, sellepärast kasutama 
-        
-        
-                
+        this._list = this._homeModule.querySelector('#restOutput');
+        let animals = new Animals();
+        this._refresh();
     }
     protected _bindEvents(){
-         this._button.addEventListener('click', this._render.bind(this));
-                                     
+        this._button.addEventListener('click', this._refresh.bind(this));
     }
-  
     protected _render(){
-        this._restAnswer = Helper.getHTMLTemplate('http://rest-service.guides.spring.io/greeting');
-        let restJSON = JSON.parse(this._restAnswer) as Greeting;   // loeb sisu JSON.parse teeb objektiks ja ütleb et loe interface formaadis
-       this._list.innerHTML = 'ID: '+ restJSON.id + ' Sisu: ' + restJSON.content;
-            }
-         
- } 
-   
+        
+        this._list.innerHTML = 'Id: '+ this._restJSON.id + 
+            ' Sisu: ' + this._restJSON.content;
+    }
+    private _refresh(){
+        let restAnswer = Helper.getHTMLTemplate('http://rest-service.guides.spring.io/greeting');
+        this._restJSON = JSON.parse(restAnswer) as Greeting;
+        this._render();
+    }
+}
